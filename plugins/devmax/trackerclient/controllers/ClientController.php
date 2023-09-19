416b423be33ce71c2d5a223f60d98975d52688ff
Maxat <code.datum@gmail.com>
2023-09-19 14:23:42 +0600

    refer_links counts logic

--- plugins/devmax/trackerclient/controllers/ClientController.php
+++ plugins/devmax/trackerclient/controllers/ClientController.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 namespace Devmax\TrackerClient\Controllers;
 
 include_once "smsc_api.php";
@@ -10,51 +9,69 @@ use Devmax\TrackerClient\Models\Clients;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Devmax\TrackerClient\Models\RefersLinks;
+use Devmax\TrackerClient\Models\Refers;
 
 
 class ClientController extends Controller
 {
-  public function client(Request $request)
-  {
-    $data = $request->all();
+    public function client(Request $request)
+    {
+        $data = $request->all();
 
+        // check first phone number in table clients if exists then +1 once in one day
+        $client = new Clients;
+        $client_phone = $client->where('phone', $data['phone'])->first();
+        \Log::info(['client phone', $data['phone']]);
+        if (!$client_phone) {
+            $client->name = $data['name'];
+            $client->phone = $data['phone'];
+            $client->refer_id = $data['utm_track_uid'];
+            $client->save();
+        } elseif ($client_phone->created_at->format('Y-m-d') == date('Y-m-d')) {
+            return response()->json(['message' => 'This phone registered today'], 200);
+        }
+        $refer_links = new RefersLinks;
+        // if utm_track_uid has exists in table refers_links then +1 to refers_links.count
+        $refer_link = $refer_links->where('uid', $data['utm_track_uid'])->first();
+        $time = date("Y-m-d H:i:s");
+        \Log::info([$time, $data['phone'], $refer_link->id, $refer_link->uid, $refer_link->counts]);
+        // if phone number different from phone number in table clients then +1 to refers.count
+        $client_phone = $client->where('phone', $data['phone'])->first();
+        if ($refer_link->uid == $client_phone->refer_id) {
+            $refer_link->counts = $refer_link->counts + 1;
+            $refer_link->save();
+        }
 
-    $client = new Clients;
-    $client->name = $data['name'];
-    $client->phone = $data['phone'];
-    $client->refer_id = $data['utm_track_uid'];
-    $client->save();
-
-    return response()->json(['message' => 'Successfully created'], 201);
-  }
-
-
-
-  public function getBalance()
-  {
-    try {
-      $balance = get_balance();
-      return response()->json(['balance' => $balance], 200);
-    } catch (Exception $e) {
-      echo "Ошибка: " . $e->getMessage() . "\n";
+        return response()->json(['message' => 'Successfully created'], 201);
     }
-  }
 
 
-  public function sendSms(Request $request)
-  {
-    $data = $request->all();
-    $phone = $data['phone'];
-    $text = $data['message'];
-    $sender = "Meyram";
-
-
-    try {
-      // return response()->json(['tel' => $phone, 'text' => $text, $sender], 200);
-      $result = send_sms($phone, $text, 0, 0, 0, 0, $sender);
-      return response()->json(['result' => $result], 200);
-    } catch (Exception $e) {
-      echo "Ошибка: " . $e->getMessage() . "\n";
+    public function getBalance()
+    {
+        try {
+            $balance = get_balance();
+            return response()->json(['balance' => $balance], 200);
+        } catch (Exception $e) {
+            echo "Ошибка: " . $e->getMessage() . "\n";
+        }
     }
-  }
+
+
+    public function sendSms(Request $request)
+    {
+        $data = $request->all();
+        $phone = $data['phone'];
+        $text = $data['message'];
+        $sender = "Meyram";
+
+
+        try {
+            // return response()->json(['tel' => $phone, 'text' => $text, $sender], 200);
+            $result = send_sms($phone, $text, 0, 0, 0, 0, $sender);
+            return response()->json(['result' => $result], 200);
+        } catch (Exception $e) {
+            echo "Ошибка: " . $e->getMessage() . "\n";
+        }
+    }
 }
