67677ef0e2c4a561b7b260897b84f341c68e02e6
Maxat Oralbayev <m.oralbaev@peopleandpeople.io>
2023-09-20 16:45:24 +0600

    client js

--- docs/tilda.js
+++ docs/tilda.js
@@ -1,48 +1,54 @@
 console.log("start client");
-window.addEventListener("load", function() {
-  const form = document.querySelector('form');
-  const submit = form.querySelector('button[type="submit"]');
+window.addEventListener("load", function () {
+    const form = document.querySelector("form");
+    const submit = form.querySelector('button[type="submit"]');
 
-  submit.addEventListener("click", function(event) {
-    event.preventDefault();
-    const nameInput = document.querySelector('input[name="Name"]');
-    const name = nameInput ? nameInput.value || "user" : "user";
+    submit.addEventListener("click", function (event) {
+        event.preventDefault();
+        const nameInput = document.querySelector('input[name="Name"]');
+        const name = nameInput ? nameInput.value || "user" : "user";
 
-    const phone = document.querySelector('input[name="Phone"]').value;
+        const phone = document.querySelector('input[name="Phone"]').value;
 
+        const hash = window.location.hash;
+        const match = hash.match(/utm_track_uid=([^&]+)/);
 
-    const hash = window.location.hash;
-    const match = hash.match(/utm_track_uid=([^&]+)/);
+        let clientUid = null;
 
-    let clientUid = null;
+        if (match) {
+            clientUid = match[1];
+        }
+        console.log("match", match, clientUid);
 
-    if (match) {
-      clientUid = match[1];
-    }
+        const data = {
+            name: name,
+            phone: phone,
+            utm_track_uid: clientUid,
+        };
+        var jsonData = JSON.stringify(data);
 
-    const data = {
-      name: name,
-      phone: phone,
-      utm_track_uid: clientUid
-    };
+        console.log("Data", jsonData);
+        const url = 'https://meyram-app.kz/api/v1/client';
+        //const url = 'https://httpbin.org/post';
+        // Create a fetch POST request
+        fetch(url, {
+            method: 'POST',
+            mode: 'no-cors', // Specify 'no-cors' mode
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: jsonData
+        })
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (responseText) {
+                // Handle the response from the PHP script
+                console.log("Response from server script: " + responseText);
+            })
+            .catch(function (error) {
+                console.error("Error sending data to server script: " + error);
+            });
 
-    console.log("Data", data);
-    const apiUrl = 'https://meyram-app.kz/htdocs/plugins/devmax/trackerclient/client/client.php';
-
-    fetch(apiUrl, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  });
-
+    });
 });
