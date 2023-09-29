8e4fd5ad94de83ad3d14ac36d0e1b945ee9d1139
Maxat <code.datum@gmail.com>
2023-09-29 14:21:07 +0600

    added success_count calculate logic

--- plugins/devmax/trackerclient/controllers/CidController.php
+++ plugins/devmax/trackerclient/controllers/CidController.php
@@ -7,6 +7,7 @@ use Devmax\TrackerClient\Models\Links;
 use Devmax\TrackerClient\Models\Message;
 use Devmax\TrackerClient\Models\Sms;
 use Illuminate\Routing\Controller;
+use Devmax\TrackerClient\Models\RefersLinks;
 
 class CidController extends Controller
 {
@@ -19,7 +20,21 @@ class CidController extends Controller
 
     $messageId = Sms::where('client_id', $clientId)->pluck('message_id')->toArray()[0];
     $bizonUrl = Message::where('id', $messageId)->pluck('url_bizon')->toArray()[0];
+    // Increment success_count if user enter from refer link
+      $client = Clients::where('id', $clientId)->get()[0];
+      $refer_links = RefersLinks::where('uid', $client->refer_id)->get()[0];
+      \Log::info(['Cid controller',$clientId, $client->refer_id, $refer_links->uid]);
+      // it should be once
 
+      if( $client->refer_id == $refer_links->uid){
+          // increment success count
+        $refer_links->success_counts = $refer_links->success_counts + 1;
+        if($refer_links->counts !=0 ) {
+            // calculate rating
+          $refer_links->rating = round($refer_links->success_counts / $refer_links->counts * 100, 2);
+        }
+        $refer_links->save();
+      }
     return redirect($bizonUrl);
 
   }
